6341c92f1e668366eeb7b76edd8f16057696df17
Uttam <uttamjangir1415@gmail.com>
2025-09-23 13:21:45 +0530

    fix #1

--- app/api/simulate/route.ts
+++ app/api/simulate/route.ts
@@ -3,14 +3,12 @@ import axios from "axios";
 
 export async function POST(req: Request) {
   try {
+    console.log(`${process.env.BACKEND_URL}`);
     const body = await req.json();
     const backendRes = await axios.post(`${process.env.BACKEND_URL}`, body);
     return NextResponse.json(backendRes.data);
   } catch (err) {
     console.error("Proxy error:", err);
-    return NextResponse.json(
-      { error: "Failed to fetch from backend" },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: `${err}` }, { status: 500 });
   }
 }
